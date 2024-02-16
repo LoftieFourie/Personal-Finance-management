542ada4ae6cbb720db5f60cc192eabc793e26ef7
Loftie Fouire <63976609+LoftieFourie@users.noreply.github.com>
2024-02-16 22:52:18 +0200

    env

--- PFM/src/app/environments/environments.ts
+++ PFM/src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/v1', // Replace with your actual API URL
-  // apiUrl: 'https://pfm-api-service.onrender.com/api/v1',
+  // apiUrl: 'http://localhost:8000/api/v1', // Replace with your actual API URL
+  apiUrl: 'https://pfm-api-service.onrender.com/api/v1',
 };
